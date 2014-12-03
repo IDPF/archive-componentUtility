919ebecbb3a21ada5c88f7415e16b5eb237889c5
ViscousDocs <wmanis@msn.com>
2014-12-03 09:36:13 -0800

    remove python binaries in clean

--- clean.sh
+++ clean.sh
@@ -5,7 +5,7 @@ find . -name *.epub | xargs rm;
 find . -name .DS_Store | xargs rm;
 find . -name *.*~ | xargs rm;
 
-
+rm -f *.pyc
 rm -rf Acme_Gallery_example.extracted
 rm -rf epub/integratedComponent
 rm -rf epub/componentContainer.merged
